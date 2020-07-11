03c715e8a78997fb7f65563d1522db37969eaada
mistymann <40823288+mistymann@users.noreply.github.com>
2020-07-11 18:59:03 +0300

    Update app.js
    abdest güncellemesi on

--- app.js
+++ app.js
@@ -10,5 +10,10 @@ client.on('message', msg => {
     msg.reply('pong');
   }
 });
+client.on('message', msg => {
+  if (msg.content === '!!abdest') {
+    msg.reply('A B D E S T L E N D İ N');
+  }
+});
 
 client.login('NzMxNTE0MzM0NjIxOTkxMDI1.XwnP2g.Hb1vCMqZ0Vto32-fu8X1kzFHN-o');
