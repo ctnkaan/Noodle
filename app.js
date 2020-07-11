3ed8c09e181d4a73b49cb9451777c710403ab524
MaViYboct <kaankaan111@gmail.com>
2020-07-11 21:12:29 +0300

    added penis size and ability to join voice channel

--- app.js
+++ app.js
@@ -6,10 +6,38 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+
+  //displays a giff img
   if (msg.content === '!!abdest') {
     msg.channel.send('A B D E S T L E N D İ N');
     msg.channel.send("https://i.pinimg.com/originals/c4/27/7d/c4277d9d382493ff8c55e975d438ed1c.gif");
   }
-});
+
+  //gives the user a number indicating their penis size between 0-45 and then comments on it
+  if (msg.content === "!!penis") {
+  var a = Math.floor(Math.random() * 45)+1;
+  msg.reply(a+"cm");
+  if (a <= 5) {
+    msg.reply("Görünmüyor")
+  } else if (a <= 15) {
+    msg.reply("Ufacık");
+  } else if (a > 15 && a<=30) {
+    msg.reply("Nice dick bro");
+  } else {
+    msg.reply("ANASKMM CANAVAAAAR");
+  }
+}
+
+ //connects to users room
+ if (message.content === '!!join') {
+   // Only try to join the sender's voice channel if they are in one themselves
+   if (message.member.voice.channel) {
+     const connection = await message.member.voice.channel.join();
+   } else {
+     message.reply('You need to join a voice channel first!');
+   }
+ }
+
+ });
 
 client.login('NzMxNTE0MzM0NjIxOTkxMDI1.XwnjCQ.GyBLDAidTTpQr28ywMobPB7W2ZY');
