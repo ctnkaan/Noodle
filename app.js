136beda79f5b3526565a993b2c0d780c6847307d
mistymann <40823288+mistymann@users.noreply.github.com>
2020-07-11 21:38:06 +0300

    roll uptade

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ client.on('ready', () => {
 client.on('message', msg => {
 
   //displays a giff img
-  
+
   if (msg.content === '!!abdest') {
     msg.channel.send('A B D E S T L E N D İ N');
     msg.channel.send("https://i.pinimg.com/originals/c4/27/7d/c4277d9d382493ff8c55e975d438ed1c.gif");
   }
 
+  if(msg.content === "!!roll"){
+    var a = Math.floor(Math.random() * 1000)+1;
+    msg.reply(a);
+
+}
   //gives the user a number indicating their penis size between 0-45 and then comments on it
 
   if (msg.content === "!!penis") {
