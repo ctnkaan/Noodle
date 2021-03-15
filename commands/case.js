ed98745f1b588af4117fb9ca9476127191c2d002
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-03-15 03:06:57 +0300

    csgo case opening finished

--- commands/case.js
+++ commands/case.js
@@ -9,20 +9,23 @@ module.exports = {
         console.log(luck);
 
         if (luck <= 60) {
-            let blue = Math.floor(Math.random() * 24) + 1;
+            let item = Math.floor(Math.random() * 24) + 1;
             msg.channel.send('You got a blue item!');
-            msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/blue/'+ blue +'.png');
-
-
-
-
+            msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/blue/'+ item +'.png');
 
         } else if (luck <= 80) {
+            let item = Math.floor(Math.random() * 10) + 1;
             msg.channel.send('You got a purple item!');
+            msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/blue/'+ item +'.png');
+
         } else if (luck <= 90) {
+            let item = Math.floor(Math.random() * 5) + 1;
             msg.channel.send('You got a red item!');
+            msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/blue/'+ item +'.png');
         } else if (luck > 90) {
+            let item = Math.floor(Math.random() * 7) + 1;
             msg.channel.send('You got a ultra rare item!');
+            msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/blue/'+ item +'.png');
         }
 
     },
