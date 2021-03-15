6620bd3a8ffdc1a0f0e2bf235aab8b85552a36b8
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-03-15 23:16:17 +0300

    added blackjack game

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const Moderation = require('./commands/moderation');
 const Case = require('./commands/case');
 
 
-let bullets = 6; // Required for Russian Rulatte
+let stack = [], playerDeck = [], cpuDeck = [], curr, cpuSum = 0, sum = 0, gameStarted = false, bullets = 6;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -39,77 +39,175 @@ client.on('message', msg => {
     bullets = Rr.execute(msg, bullets);
   }
 
-
-
   //Stats
   else if (command === "stats") {
     Stats.execute(msg, client);
   }
-
-
 
   //Bless
   else if (command === 'bless') {
     Bless.execute(msg);
   }
 
-
-
   //Help
   else if (command === 'help') {
     Help.execute(msg);
   }
-
-
 
   //Rock paper scissors
   else if (command === 'rps') {
     Rps.execute(msg);
   }
 
-
-
   //Countdown
   else if (command === "countdown") {
     Countdown.execute(msg, args);
   }
-
-  
 
   //Roll
   else if (command === 'roll') {
     Roll.execute(msg, args);
   }
 
-
-
   //Reddit
   else if (command === 'reddit') {
     Reddit.execute(msg, args);
   }
-
-
 
   //Meme
   else if (command === "meme") { 
     Meme.execute(msg);
   }
 
-
-
   //Moderation
   else if (command === "kick" || command === "ban") {
     Moderation.execute(msg, command);
   }
 
-
-
   //CSGO Case
   else if (command === "case") {
     Case.execute(msg);
   }
+
+  
+//////////////////////////////////Blackjack////////////////////////////////////////
+
+  else if (command === "bj") {
+
+    gameStarted = true;
+
+    for (let i = 0; i < 13; i++) {
+        for (let j = 1; j <= 13; j++) {
+            if (j > 10) 
+              stack.push(10);
+            else 
+              stack.push(j);
+        }
+    }
+
+    shuffle(stack)
+
+    console.log(stack);
+
+    //setting up players hand
+    for (let i = 0; i < 2; i++) {
+        curr = stack.pop()
+        playerDeck[i] = curr;
+    }
+
+    //setting up cpu hand
+    for (let i = 0; i < 2; i++) {
+        curr = stack.pop()
+        cpuDeck[i] = curr;
+    }
+
+    console.log(cpuDeck);
+
+    msg.channel.send("Cpu has "+ cpuDeck[0]+ " and a hidden card\n\nYou have "+playerDeck[0]+" and "+playerDeck[1]+"\n\nType !!h for Hit\nType !!s for Stay");
+  }
+  
+  else if (command === "h" && gameStarted === true) {
+    curr = stack.pop();
+    console.log(curr);
+    playerDeck.push(curr);
+
+    sum=0;
+    for (let i = 0; i < playerDeck.length; i++)
+      sum += playerDeck[i];
+
+    msg.channel.send("You draw "+curr+"\nYour current sum is "+sum);
+
+    if (sum > 21) {
+      msg.channel.send("Bust!");
+      stack = [], playerDeck = [], cpuDeck = [], curr, sum = 0;
+      gameStarted = false;
+    }
+  }
+
+  else if (command == "s" && gameStarted == true) {
+    msg.channel.send("CPU has "+ cpuDeck[0] + " and "+ cpuDeck[1]);
+
+    if (cpuSum > 21) {
+      msg.channel.send("CPU Bust");
+      stack = [], playerDeck = [], cpuDeck = [], curr, sum = 0;
+      gameStarted = false;
+    } 
+
+    while(gameStarted == true) {
+      curr = stack.pop();
+      console.log(curr);
+      cpuDeck.push(curr);
+
+      cpuSum=0;
+      for (let i = 0; i < cpuDeck.length; i++) 
+        cpuSum+= cpuDeck[i];
+      
+        
+      msg.channel.send("CPU draws "+curr+"\nCurrent CPU sum is "+cpuSum);
+
+      if (cpuSum > 21) {
+        msg.channel.send("CPU Bust !");
+        stack = [], playerDeck = [], cpuDeck = [], curr, sum = 0;
+        gameStarted = false;
+        break;
+
+      } else if (cpuSum > sum) {
+        console.log(cpuSum);
+        msg.channel.send("CPU wins !");
+        stack = [], playerDeck = [], cpuDeck = [], curr, sum = 0;
+        gameStarted = false;
+        break;
+
+      } else {
+        msg.channel.send("Stalemate !");
+        stack = [], playerDeck = [], cpuDeck = [], curr, sum = 0;
+        gameStarted = false;
+      }
+    }
+  }
+
+/////////////////////////////////Blackjack//////////////////////////////////////
+  
 });
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
   
 
