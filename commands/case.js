ffa9281e8445626e80d38aec147c919292dc7130
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-03-15 04:14:25 +0300

    changed some values for testig. might change again depending on user feedback

--- commands/case.js
+++ commands/case.js
@@ -5,24 +5,24 @@ module.exports = {
 
         
 
-        let luck = Math.floor(Math.random() * 100) + 1;
+        let luck = Math.floor(Math.random() * 300) + 1;
         console.log(luck);
 
-        if (luck <= 60) {
+        if (luck <= 250) {
             let item = Math.floor(Math.random() * 24) + 1;
-            msg.channel.send('You got a blue item!');
+            msg.channel.send('You got a blue item!\n ');
             msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/blue/'+ item +'.png');
 
-        } else if (luck <= 80) {
+        } else if (luck <= 260) {
             let item = Math.floor(Math.random() * 10) + 1;
             msg.channel.send('You got a purple item!');
             msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/purple/'+ item +'.png');
 
-        } else if (luck <= 90) {
+        } else if (luck <= 270) {
             let item = Math.floor(Math.random() * 5) + 1;
             msg.channel.send('You got a red item!');
             msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/red/'+ item +'.png');
-        } else if (luck > 90) {
+        } else if (luck > 270) {
             let item = Math.floor(Math.random() * 7) + 1;
             msg.channel.send('You got a ultra rare item!');
             msg.channel.send('https://raw.githubusercontent.com/ctnkaan/Noodle/dev/images/items/yellow/'+ item +'.png');
