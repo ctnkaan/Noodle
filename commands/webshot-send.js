c5d9eaca37cc4ee318b47d3c97146d0b5b03c5c5
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2021-06-02 00:22:48 +0300

    fixed webshot-send

--- commands/webshot-send.js
+++ commands/webshot-send.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
     name: 'webshot-send',
     description: 'send captured img',
@@ -8,6 +10,11 @@ module.exports = {
               attachment: "img.png",
               name: "img.png"
             }]
+          }).then( () => {
+            fs.unlink('./img.png', (err) => {
+              if (err) throw err;
+              console.log('deleted');
+            });
           })
         } catch (err) {
           msg.channel.send(err);
--- commands/webshot.js
+++ commands/webshot.js
@@ -8,4 +8,4 @@ module.exports = {
             console.log(err);
          });
     },
-   };
+  };
